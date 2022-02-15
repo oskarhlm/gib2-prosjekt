2d7a73c47a3a1d266cd3a35127af44ba76460ee6
Karl Oskar Magnus Holm <koholm@stud.ntnu.no>
2022-02-15 18:37:29 +0100

    fix form

--- frontend/src/components/Map.tsx
+++ frontend/src/components/Map.tsx
@@ -5,7 +5,7 @@ export function Map() {
   return (
     <div>
       <SettingsDrawer />
-      <MapContainer center={[51.505, -0.09]} zoom={13} zoomControl={false}>
+      <MapContainer center={[63.4346, 10.3985]} zoom={13} zoomControl={false}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
--- frontend/src/components/SettingsDrawer.tsx
+++ frontend/src/components/SettingsDrawer.tsx
@@ -1,6 +1,15 @@
 import { useState } from 'react';
-import { Button, Drawer, Slider, Form } from 'antd';
+import {
+  Button,
+  Drawer,
+  Slider,
+  Form,
+  Radio,
+  Checkbox,
+  TimePicker,
+} from 'antd';
 import { MenuOutlined } from '@ant-design/icons';
+import moment from 'moment';
 
 export function SettingsDrawer() {
   const [collapsed, setCollapsed] = useState(false);
@@ -9,8 +18,9 @@ export function SettingsDrawer() {
     setCollapsed(!collapsed);
   };
 
-  const handleSubmit = () => {
-    console.log('Success');
+  const handleSubmit = (values: any) => {
+    console.log('Values recieved: ', values);
+    handleCollapsed();
   };
 
   return (
@@ -25,16 +35,43 @@ export function SettingsDrawer() {
         <MenuOutlined />
       </Button>
       <Drawer
-        title="Innstillinger"
+        title="Innstillinger for tur"
         placement={'left'}
         closable={true}
         onClose={handleCollapsed}
         visible={collapsed}
         key={1}
       >
-        <Form onFinish={handleSubmit}>
-          <Form.Item label="Maks radius (km)">
-            <Slider max={5} step={0.1} />
+        <Form
+          onFinish={handleSubmit}
+          initialValues={{
+            roundTrip: false,
+            latestFinishTime: moment().add(2, 'h'),
+            maxRadius: 2,
+            maxSlope: 30,
+            experienceLevel: '3',
+          }}
+        >
+          <Form.Item name="roundTrip" valuePropName="checked">
+            <Checkbox>Tur/retur</Checkbox>
+          </Form.Item>
+          <Form.Item name="latestFinishTime" label="Tur ferdig innen">
+            <TimePicker showNow={false} />
+          </Form.Item>
+          <Form.Item name="maxRadius" label="Maks radius (km)">
+            <Slider max={10} step={0.1} />
+          </Form.Item>
+          <Form.Item name="maxSlope" label="Maks helning (%)">
+            <Slider max={30} />
+          </Form.Item>
+          <Form.Item name="experienceLevel" label="Erfaringsnivå">
+            <Radio.Group>
+              <Radio.Button value="1">1</Radio.Button>
+              <Radio.Button value="2">2</Radio.Button>
+              <Radio.Button value="3">3</Radio.Button>
+              <Radio.Button value="4">4</Radio.Button>
+              <Radio.Button value="5">5</Radio.Button>
+            </Radio.Group>
           </Form.Item>
           <Form.Item wrapperCol={{ span: 12, offset: 0 }}>
             <Button type="primary" htmlType="submit">
