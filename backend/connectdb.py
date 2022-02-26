359355731266e8edc3c23c9a90a136370defd50d
Karl Oskar Magnus Holm <koholm@stud.ntnu.no>
2022-02-26 14:01:48 +0100

    fix json query

--- backend/connectdb.py
+++ backend/connectdb.py
@@ -2,6 +2,7 @@ import psycopg2
 import json
 from config import config
 
+
 def connect():
     conn = None
     try:
@@ -9,12 +10,18 @@ def connect():
         print("connecting...")
         conn = psycopg2.connect(**params)
         cur = conn.cursor()
-        #cur.execute('SELECT version()')
-        cur.execute('select geom::geometry::json from vbase_50_punkt limit 3;')
-        rows = cur.fetchall()
-        for row in rows:
-            y = json.loads(row)
-            print(y)
+        cur.execute('SELECT version()')
+        cur.execute("select json_agg(ST_AsGeoJSON(s.*)::json) \
+                    from (select fartsgrens, typeveg, slope, geom from streets limit 3) as s;")
+        # cur.execute("SELECT jsonb_build_object( \
+        #                 'type',       'Feature', \
+        #                 'id',         id, \
+        #                 'geometry',   ST_AsGeoJSON(geom)::jsonb, \
+        #                 'properties', to_jsonb( t.* ) - 'id' - 'geom' \
+        #                 ) AS json \
+        #             FROM point AS t(id, name, geom);")
+        rows = cur.fetchone()[0]
+        print(json.dumps(rows, indent=4))
         cur.close()
 
     except(Exception, psycopg2.DatabaseError) as error:
@@ -23,6 +30,7 @@ def connect():
         if conn is not None:
             conn.close()
             print("Connection closed")
+
 
 if __name__ == '__main__':
     connect()
